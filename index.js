34c6000030790c00c5656656cff6b333cc939e7b
itwithlyam <64089164+itwithlyam@users.noreply.github.com>
2021-08-05 16:30:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const { Client } = require("./client/gateway")
+const { token } = require("./token.json")
 
 const client = new Client()
 client.on("message", message => {
     console.log("message")
 })
 
-client.login("token")
+client.login(token)
